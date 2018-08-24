3e2f5a77d64e4aebb476d8d67cea385e77bb14af
Stanislau Yudzitski <stanislauy@playtika.com>
2018-08-24 13:26:02 +0300

    Multiexport

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-module.exports.deepFlatten = function getFlatArray(arr) {
-  const deepFlatten = arr => [].concat(...arr.map(v => (Array.isArray(v) ? deepFlatten(v) : v)));
-  return deepFlatten;
+module.exports.tiberium = {
+  getFlatArray: function (arr) {
+    const deepFlatten = arr => [].concat(...arr.map(v => (Array.isArray(v) ? deepFlatten(v) : v)));
+    return deepFlatten;
+  },
+  returnValue: function (val) {
+    return val;
+  }
 };
