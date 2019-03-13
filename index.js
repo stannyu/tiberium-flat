497c15a02c4dc940abcc990f32e667cc7a07a5fe
standman <stanislau.yudzitski@mendix.com>
2019-03-13 11:40:04 +0100

    Bifurcate added. NPM version updated.

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 module.exports.tiberium = {
-  getFlatArray: function (arr) {
-    const deepFlatten = arr => [].concat(...arr.map(v => (Array.isArray(v) ? deepFlatten(v) : v)));
+  getFlatArray: function(arr) {
+    const deepFlatten = arr =>
+      [].concat(...arr.map(v => (Array.isArray(v) ? deepFlatten(v) : v)));
     return deepFlatten;
   },
-  returnValue: function (val) {
+  returnValue: function(val) {
     return val;
+  },
+  bifurcate: function(arr, filter) {
+     return arr.reduce((acc, val, i) => (acc[filter[i] ? 0 : 1].push(val), acc), [
+        [],
+        []
+      ]);
   }
 };
