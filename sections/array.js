6a3917bba7c26e0bf0f60d829a1e0fb8f81302da
standman <stanislau.yudzitski@mendix.com>
2019-03-27 10:07:44 +0100

    Last method added.

--- sections/array.js
+++ sections/array.js
@@ -481,6 +481,15 @@ const tfArray = {
           : i === arr.length - 1 ? acc + val : acc + val + separator,
       ''
     ),
+  /**
+   *
+   * @param arr {Array<any>}
+   * @return {any}
+   *
+   * Returns the last element in an array.
+   * Use arr.length - 1 to compute the index of the last element of the given array and returning it.
+   */
+  last: arr => arr[arr.length - 1],
 };
 
 module.exports = tfArray;
