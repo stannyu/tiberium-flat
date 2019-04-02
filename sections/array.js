c11766270b6df519e1eb2bd3e159a32309c2eeae
standman <stanislau.yudzitski@mendix.com>
2019-04-02 09:26:38 +0200

    MinN method added.

--- sections/array.js
+++ sections/array.js
@@ -535,6 +535,21 @@ const tfArray = {
    * Omit the second argument, n, to get a one-element array.
    */
   maxN: (arr, n = 1) => [...arr].sort((a, b) => b - a).slice(0, n),
+  /**
+   *
+   * @param arr
+   * @param n
+   * @return {any[]}
+   *
+   * Returns the n minimum elements from the provided array. If n is greater than or equal
+   * to the provided array's length, then return the original array(sorted in ascending order).
+   *
+   * Use Array.sort() combined with the spread operator (...) to create a shallow clone of the array and sort
+   * it in ascending order.
+   * Use Array.slice() to get the specified number of elements.
+   * Omit the second argument, n, to get a one-element array.
+   */
+  minN: (arr, n = 1) => [...arr].sort((a, b) => a - b).slice(0, n),
 };
 
 module.exports = tfArray;
