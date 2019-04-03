8c1f8cd0de15fbbf717626a2913aef0b4e493350
standman <stanislau.yudzitski@mendix.com>
2019-04-03 16:09:58 +0200

    PullAtIndex method added.

--- sections/array.js
+++ sections/array.js
@@ -596,7 +596,27 @@ const tfArray = {
     let pulled = arr.filter((v, i) => !argState.includes(v));
     arr.length = 0;
     pulled.forEach(v => arr.push(v));
-  }
+  },
+  /**
+   *
+   * @param arr
+   * @param pullArr
+   * @return {Array}
+   *
+   * Mutates the original array to filter out the values at the specified indexes.
+   * Use Array.filter() and Array.includes() to pull out the values that are not needed.
+   * Use Array.length = 0 to mutate the passed in an array by resetting it's length to zero
+   * and Array.push() to re-populate it with only the pulled values. Use Array.push() to keep track of pulled values
+   */
+  pullAtIndex: (arr, pullArr) => {
+    let removed = [];
+    let pulled = arr
+      .map((v, i) => (pullArr.includes(i) ? removed.push(v) : v))
+      .filter((v, i) => !pullArr.includes(i));
+    arr.length = 0;
+    pulled.forEach(v => arr.push(v));
+    return removed;
+  },
 };
 
 module.exports = tfArray;
