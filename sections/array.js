48c3005bd347cdaa54ac674d923fa9a6baf40d97
standman <stanislau.yudzitski@mendix.com>
2019-04-11 09:36:14 +0200

    reduceSuccessive method added.

--- sections/array.js
+++ sections/array.js
@@ -680,6 +680,22 @@ const tfArray = {
         acc[key] = el[key];
         return acc;
       }, {})
+    ),
+  /**
+   *
+   * @param arr
+   * @param fn
+   * @param acc
+   * @return {*}
+   *
+   * Applies a function against an accumulator and each element in the array (from left to right),
+   * returning an array of successively reduced values.
+   * Use Array.reduce() to apply the given function to the given array, storing each new result.
+   */
+  reduceSuccessive: (arr, fn, acc) =>
+    arr.reduce(
+      (res, val, i, arr) => (res.push(fn(res.slice(-1)[0], val, i, arr)), res),
+      [acc]
     )
 };
 
